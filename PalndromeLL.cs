ada1c904702c848c25e1a40750eb629953459b55
Jamesaustin1997 <j.austin1997.ja@gmail.com>
2022-08-27 00:19:14 +1000

    create project

---
+++ PalndromeLL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+        public class Solution
+        {
+            public static void Main()
+            {
+                Solution s = new Solution();
+                s.IsPalindrome(new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(2, new ListNode(1, new ListNode(0))))))))); // returns true
+                s.IsPalindrome(new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(2, new ListNode(5, new ListNode(0))))))))); // returns false
+            }
+
+
+            public bool IsPalindrome(ListNode head)
+            {
+                var values = new List<int>();
+                while(head != null)
+                {
+                    values.Add(head.val);
+                    head = head.next;
+                }
+
+                int x = 0;
+                int end = values.Count() - 1;
+                while (x < end)
+                {
+                    if (values[x] != values[end])
+                        return false;
+                    x++;
+                    end--;
+                }
+
+                int[] a = new int[1];
+
+
+               return true;
+            }
+        }
+    }
+}
---
+++ RomanToInt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            var dictionary = new Dictionary<char, int>();
+
+            dictionary.Add('I', 1);
+            dictionary.Add('V', 5);
+            dictionary.Add('X', 10);
+            dictionary.Add('L', 50);
+            dictionary.Add('C', 100);
+            dictionary.Add('D', 500);
+            dictionary.Add('M', 1000);
+
+            int solution = 0;
+
+            s = s.Replace("IV", "IIII")
+                .Replace("IX", "VIIII")
+                .Replace("XL", "XXXX")
+                .Replace("XC", "LXXXX")
+                .Replace("CD", "CCCC")
+                .Replace("CM", "DCCCC");
+
+            foreach (char c in s)
+            {
+                solution += dictionary[c];
+            }
+
+            return solution;
+        }
+    }
+}
